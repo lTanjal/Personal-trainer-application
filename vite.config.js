f8b65b22f2e1b2abf29a35dd1249c3c4f153b772
Tatiana <tatiana.lyubavskaya@gmail.com>
2024-04-19 10:32:39 +0300

    Deployment changes

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-   base: "/PersonalTrainerApplication/",
+    plugins: [react()],
+    base: '/PersonalTrainerApplication/',
   plugins: [react()],
- 
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
 })
